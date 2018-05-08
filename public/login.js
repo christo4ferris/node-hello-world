32949b1bd378fe2675cf3322975a2667d72ef035
Swetha Repakula <srepaku@us.ibm.com>
2018-05-07 18:55:33 -0700

    Add script to check password and go to vote page
     - currently password is some-password to test

---
+++ public/login.js
@@ -0,0 +1,18 @@
+function SetAccountCookie(username,password){
+  var d = new Date();
+  d.setTime(d.getTime() + (1*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = "ETHAccount" + "=" + username + ";" + expires + ";path=/";
+}
+function login(doc){
+  accountAddr = document.getElementById('accountAddr').value
+  password = document.getElementById('password').value
+
+  if (password != "some-password"){
+    console.log("incorrect-password")
+    return
+  }
+
+  SetAccountCookie(accountAddr, password)
+  window.location.href = "vote.html"
+}
