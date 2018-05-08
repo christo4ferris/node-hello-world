8f1dcc3ba3100d42262392aab205e6d0639d5f77
Christopher Ferris <chrisfer@us.ibm.com>
2018-05-08 10:42:40 -0400

    merged with jay's changes
    Signed-off-by: Christopher Ferris <chrisfer@us.ibm.com>

--- public/login.js
+++ public/login.js
@@ -2,7 +2,8 @@ function SetAccountCookie(username,password){
   var d = new Date();
   d.setTime(d.getTime() + (1*24*60*60*1000));
   var expires = "expires="+ d.toUTCString();
-  document.cookie = "ETHAccount=" + username + ";" + expires + ";path=/";
+  var cookie = "ETHAccount=" + username + ";" + expires + ";path=/";
+  document.cookie = cookie;
 }
 function login(doc){
   accountAddr = document.getElementById('accountAddr').value
